e98295ddb242c7f2c72eca642f03d167da2cb7b2
Zoli <zoltan.konradi@gmail.com>
2019-07-02 17:03:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    /*
+
+     Cracking the Coding Interview - Linked Lists
+
+     2.7 - Intersection:
+     Given two (singly) linked lists, determine if the two lists intersect. Return the inter­
+     secting node. Note that the intersection is defined based on reference, not value. That is, if the kth
+     node of the first linked list is the exact same node (by reference) as the jth node of the second
+     linked list, then they are intersecting.
+    */
+
+    public static void main(String[] args) {
+        List<Integer> list1 = new LinkedList<>();
+        List<Integer> list2 = new LinkedList<>();
+        List<Integer> list3 = new LinkedList<>();
+        Integer commonNumber = new Integer(5);
+        list1.add(new Integer(2));
+        list1.add(new Integer(3));
+        list1.add(commonNumber);
+        list2.add(new Integer(4));
+        list2.add(commonNumber);
+        list2.add(new Integer(4));
+        list3.add(new Integer(4));
+        list3.add(new Integer(2));
+        list3.add(new Integer(1));
+        System.out.println(isIntersection(list1, list2)); // true
+        System.out.println(isIntersection(list1, list3)); // false
+    }
+
+    public static boolean isIntersection(List<Integer> list1, List<Integer> list2) {
+        for (Integer object1 : list1) {
+            for (Integer object2 : list2) {
+                if (object1 == object2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
